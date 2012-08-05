5e88877a5d8f8e61deda641c8ae16700f71e92f9
denis <denis@localhost>
2012-08-05 15:36:45 -0400

    Denis: Created The C++ Client Project in NetBeans 7.2

---
+++ Ueki_Client/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: denis
+ *
+ * Created on August 5, 2012, 3:34 PM
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
