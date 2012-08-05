9cb51449aaccb16e6342dcfd25d5452505650337
denis <denis@localhost>
2012-08-05 16:03:28 -0400

    Denis:some changes

--- Ueki_Client/main.cpp
+++ Ueki_Client/main.cpp
@@ -7,12 +7,15 @@
 
 #include <QtGui/QApplication>
 
+#include "Forms/MainMenu.h"
+
 int main(int argc, char *argv[]) {
     // initialize resources, if needed
     // Q_INIT_RESOURCE(resfile);
 
     QApplication app(argc, argv);
-
+    MainMenu *mainMenu = new MainMenu();
+    mainMenu->show();
     // create and show your widgets here
 
     return app.exec();
---
+++ Ueki_Client/ui_MainMenu.h
@@ -0,0 +1,57 @@
+/********************************************************************************
+** Form generated from reading UI file 'MainMenu.ui'
+**
+** Created
+**      by: Qt User Interface Compiler version 4.7.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINMENU_H
+#define UI_MAINMENU_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDialog>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainMenu
+{
+public:
+    QLabel *Movie;
+
+    void setupUi(QDialog *MainMenu)
+    {
+        if (MainMenu->objectName().isEmpty())
+            MainMenu->setObjectName(QString::fromUtf8("MainMenu"));
+        MainMenu->resize(772, 570);
+        MainMenu->setContextMenuPolicy(Qt::DefaultContextMenu);
+        Movie = new QLabel(MainMenu);
+        Movie->setObjectName(QString::fromUtf8("Movie"));
+        Movie->setGeometry(QRect(250, 270, 241, 81));
+
+        retranslateUi(MainMenu);
+
+        QMetaObject::connectSlotsByName(MainMenu);
+    } // setupUi
+
+    void retranslateUi(QDialog *MainMenu)
+    {
+        MainMenu->setWindowTitle(QApplication::translate("MainMenu", "Ueki Battle Ground", 0, QApplication::UnicodeUTF8));
+        Movie->setText(QApplication::translate("MainMenu", "TextLabel", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainMenu: public Ui_MainMenu {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINMENU_H
