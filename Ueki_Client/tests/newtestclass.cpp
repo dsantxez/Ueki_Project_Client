e656e2d2f7c85e0be5d2ddd5f5c46381b6403614
denis <darzalluz@gmail.com>
2012-08-09 14:26:15 -0400

    Denis: Added Unit Test

---
+++ Ueki_Client/tests/newtestclass.cpp
@@ -0,0 +1,32 @@
+/*
+ * File:   newtestclass.cpp
+ * Author: denis
+ *
+ * Created on Aug 9, 2012, 2:25:31 PM
+ */
+
+#include "newtestclass.h"
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(newtestclass);
+
+newtestclass::newtestclass() {
+}
+
+newtestclass::~newtestclass() {
+}
+
+void newtestclass::setUp() {
+}
+
+void newtestclass::tearDown() {
+}
+
+void newtestclass::testMethod() {
+    CPPUNIT_ASSERT(true);
+}
+
+void newtestclass::testFailedMethod() {
+    CPPUNIT_ASSERT(false);
+}
+
